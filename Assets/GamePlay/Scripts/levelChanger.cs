1087ef47496db42c1a5172f94a059d44d88957c0
taimoor37-byte <taimoorhelper37@gmail.com>
2025-12-22 13:15:16 +0500

    lvl 25

---
+++ Assets/GamePlay/Scripts/levelChanger.cs
@@ -0,0 +1,13 @@
+using Core.DB.Variables;
+using UnityEngine;
+
+public class levelChanger : MonoBehaviour
+{
+    [SerializeField] int NextLvl;
+
+    private void OnEnable()
+    {
+        DBVariablesHolder.LvlIndex.Value = NextLvl;
+        DBVariablesHolder.LvlNum.Value += NextLvl;
+    }
+}
