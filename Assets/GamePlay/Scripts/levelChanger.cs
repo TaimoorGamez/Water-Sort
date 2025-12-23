f26485ff55062b790aea0a47cbf8ea3f87b8412d
taimoor37-byte <taimoorhelper37@gmail.com>
2025-12-23 15:02:38 +0500

    lvl 1 update

--- Assets/GamePlay/Scripts/levelChanger.cs
+++ Assets/GamePlay/Scripts/levelChanger.cs
@@ -9,5 +9,6 @@ public class levelChanger : MonoBehaviour
     {
         DBVariablesHolder.LvlIndex.Value = NextLvl;
         DBVariablesHolder.LvlNum.Value = NextLvl;
+        DBVariablesHolder.MaxLvlCount.Value = NextLvl;
     }
 }
